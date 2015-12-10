77232a399fbd6fdf960b65127d57994fa41eea29
Anna Zhu <zhuanna93@gmail.com>
2015-12-09 18:05:26 -0800

    Added initial project files. * Added basic Redux / React app * Added webpack to transpile React & ES6

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json']
+  }
+}
