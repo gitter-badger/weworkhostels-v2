9a939e6b91a18de99932c24d420ffb8cfb0f0630
Anna Zhu <zhuanna93@gmail.com>
2016-01-03 14:22:28 -0800

    Handle job posting a job by dispatching an action * Removed hard-coded job object in actions/addJob.js

--- src/client/components/Post.js
+++ src/client/components/Post.js
@@ -1,12 +1,15 @@
 import React, { Component, PropTypes } from 'react'
 
 class Post extends Component {
+  handleSubmit() {
+    this.props.actions.addJob({"id": 23, "title": "Programmer"})
+  }
   render() {
     const { actions } = this.props
     return (
       <div>
         <h1>POST IT HERE</h1>
-        <button onClick={actions.addJob}>Add Jobs</button>
+        <button onClick={this.handleSubmit.bind(this)}>Add Jobs</button>
       </div>
     )
   }
