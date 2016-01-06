5c98e34d66742ae4a0e2830603e52eabbe47a479
Anna Zhu <zhuanna93@gmail.com>
2016-01-05 19:50:21 -0800

    Updated 'Post' component to write data to Firebase when button is clicked. * Added form and input elements. * The 'Post' component state now stores the input value.

--- src/client/actions/addJob.js
+++ src/client/actions/addJob.js
@@ -1,6 +1,6 @@
 export function addJob(jobData) {
   return {
     type: 'ADD_JOB',
-    job: {"id": 23, "title": "Programmer"}
+    job: jobData
   }
 }
--- src/client/components/Post.js
+++ src/client/components/Post.js
@@ -1,16 +1,45 @@
 import React, { Component, PropTypes } from 'react'
+import Firebase from 'firebase'
+
+const JobListingsRef = new Firebase('https://weworkhostels-v2.firebaseio.com/job-listings')
 
 class Post extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newJob: ''
+    }
+  }
+  componentWillMount() {
+    JobListingsRef.on('child_added', (snapshot) => {
+      this.props.actions.addJob(snapshot.val())
+    })
+  }
   handleSubmit() {
-    this.props.actions.addJob({"id": 23, "title": "Programmer"})
+    const id = Math.random().toString(36).substring(7)
+    const jobRef = JobListingsRef.child(id)
+
+    jobRef.set({ // triggers child_added
+      id,
+      title: this.state.newJob,
+      time: new Date().getTime()
+    })
+    this.setState({newJob: ''})
   }
   render() {
     const { actions } = this.props
     return (
-      <div>
+      <form>
         <h1>POST IT HERE</h1>
+        <input
+          type="text"
+          value={this.state.newJob}
+          onChange={(event) => {
+            let newJob = event.target.value
+            this.setState({newJob}) }
+          } />
         <button onClick={this.handleSubmit.bind(this)}>Add Jobs</button>
-      </div>
+      </form>
     )
   }
 }
