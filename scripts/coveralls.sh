83383cdc877cd9eb033521d1c16a3aa8f206b30f
hzhu <hz@henryzhu.me>
2016-01-13 00:52:11 -0800

    Fixed an issue where mocha/babel could not compile the '@import' css statement in our JavaScript. See: https://github.com/hzhu/weworkhostels-v2/issues/9
    * Added an mocha.opts file to call some JavaScript files that fix our issue: mods/babelhook.js & mods/css-null-compiler.js
    * Moved React.Componenet tests into test/componenets.
    * Updated test.sh and coveralls.sh accordingly

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -4,6 +4,6 @@ set -e
 
 babel src --presets es2015 --out-dir ./ci-test/src
 
-babel test --presets es2015 --out-dir ./ci-test/test
+babel test/unit --presets es2015 --out-dir ./ci-test/test/unit
 
-istanbul cover ./node_modules/.bin/tape ./ci-test/test/*.js && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+istanbul cover ./node_modules/.bin/tape ./ci-test/test/unit/*.js && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,8 @@
 set -e
 
 # Testing React Components
-mocha src/client/js/components/__tests__/*.js --compilers js:babel-core/register
+mocha test/components/*.js --compilers js:babel-core/register --opts ./test/components/mocha.opts 
 
 # Testing UI JavaScript
 tape test/*.js -r babel-core/register
+tape test/unit/*.js -r babel-core/register
