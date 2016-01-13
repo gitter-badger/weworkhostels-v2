ae89fce5b0d86c3e3a1be710a94be6dc83550ecd
hzhu <hz@henryzhu.me>
2016-01-13 00:52:11 -0800

    Fixed an issue where mocha/babel could not compile the '@import' css statement in our JavaScript See: https://github.com/hzhu/weworkhostels-v2/issues/9
    * Added an mocha.opts file to call some JavaScript files that fix our issue: mods/babelhook.js & mods/css-null-compiler.js
    * Moved React.Componenet tests into test/componenets.
    * Updated test.sh and coveralls.sh accordingly

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Transpile ES6 source code
 babel src --presets es2015 --out-dir ./ci-test/src
 
-babel test --presets es2015 --out-dir ./ci-test/test
+# Transpile ES6 unit tests
+babel test/unit --presets es2015 --out-dir ./ci-test/test/unit
 
-istanbul cover ./node_modules/.bin/tape ./ci-test/test/*.js && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+# Computes statement, line, function and branch coverage with Istanbul.
+# Then pipe the results to Coveralls.
+istanbul cover ./node_modules/.bin/tape ./ci-test/test/unit/*.js && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
--- test/components/About-test.js
+++ test/components/About-test.js
@@ -5,14 +5,14 @@ import {createRenderer} from 'react-addons-test-utils'
 import expectJSX from 'expect-jsx'
 expect.extend(expectJSX)
 
-import About from '../About.js'
+import About from '../../src/client/js/components/About.js'
 
 describe('About', () => {
   it('works', () => {
     let renderer = createRenderer()
     renderer.render(<About />)
     let actualElement = renderer.getRenderOutput()
-    let expectedElement = <div> HELLO I AM ABOUT</div>
+    let expectedElement = <div className="about"><h1>HELLO I AM ABOUT</h1></div>
     expect(actualElement).toEqualJSX(expectedElement)
   })
 })
