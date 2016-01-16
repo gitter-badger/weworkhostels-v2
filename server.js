9ffdc64789bc4a7496d05f52dd01a5d31fefedf9
Anna Zhu <zhuanna93@gmail.com>
2016-01-15 16:11:39 -0800

    Replaced index.html with index.jade and updated Express server accordingly.

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 import express from 'express'
+import jade from 'jade'
 const app = express()
 
+// Set static assets directory
 app.use(express.static(__dirname))
 
+// Set Jade templates
+app.set('views', __dirname + '/src/client/templates')
+app.set('view engine', 'jade')
+
+// Set routes
 app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.render('index')
 })
 
+// Start server
 const PORT = process.env.PORT || 1337
-
 const server = app.listen(PORT, () => {
   const port = server.address().port
   console.log('Listening at http://localhost:%s', port)
