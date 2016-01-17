34040850525aa13c0ea24057be8be68056333f5c
hzhu <hz@henryzhu.me>
2016-01-16 23:00:28 -0800

    Refactored {list} in <JobListings /> component. Each item is now a <JobListItem /> component

--- src/client/js/components/JobListings.js
+++ src/client/js/components/JobListings.js
@@ -1,29 +1,45 @@
 import React, { Component, PropTypes } from 'react'
 
 class JobListings extends Component {
-  getDate(epoch) {
-    var humanDate = new Date(epoch)
-    return humanDate.toDateString()
-  }
   render() {
     const { jobs } = this.props
     const { actions } = this.props
     const list = this.props.jobs.list.map((jobObject, index) =>
-      <li key={index}>
-        {this.getDate(jobObject.createDate)} {jobObject.title} at {jobObject.name}
-      </li>
+      <JobListItem key={index} 
+                   index={index} 
+                   date={jobObject.createDate} 
+                   title={jobObject.title} 
+                   name={jobObject.name}/>
     )
 
     return (
-      <div><h1>List of jobs here</h1>
+      <div>
+        <h1>Job Listings</h1>
         {list}
       </div>
     )
   }
 }
 
-// JobListings.propTypes = {
-//   jobListings: PropTypes.array.isRequired
-// }
+class JobListItem extends Component {
+  getDate(epoch) {
+    var humanDate = new Date(epoch)
+    return humanDate.toDateString()
+  }
+  render() {
+    return (
+      <li>
+        {this.getDate(this.props.date)} | {this.props.title} at {this.props.name}
+      </li>
+    )
+  }
+}
+
+
+
+JobListings.propTypes = {
+  jobs: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
+}
 
 export default JobListings
