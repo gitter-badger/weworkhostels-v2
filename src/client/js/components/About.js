a8d4f612f9b32b4f9e4ca4d88a853c2282318a3f
Anna Zhu <zhuanna93@gmail.com>
2016-01-19 17:28:17 -0800

    Added website description to <About /> & updated unit test for <About /> component.

--- src/client/js/components/About.js
+++ src/client/js/components/About.js
@@ -3,7 +3,15 @@ import scss from '../../scss/base.scss'
 class About extends Component {
   render() {
     return (
-      <div className="about"><h1>HELLO I AM ABOUT</h1></div>
+      <div className="about">
+        <h1>About Weworkhostels</h1>
+
+        <h2>Hostels</h2>
+        <p>Find amazing volunteers for your backpacker hostel. Offer a work exchange position at your hostel!</p>
+
+        <h2>Volunteers</h2>
+        <p>Find great places to travel to, live, and work! Check out the volunteer positions posted by hostels.</p>
+      </div>
     )
   }
 }
