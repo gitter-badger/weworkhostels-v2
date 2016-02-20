a8a2f3a150410a665a521f9455d52d66350885c8
Anna Zhu <zhuanna93@gmail.com>
2016-02-19 23:16:36 -0800

    Added styles to <Preview /> component.

--- src/client/js/components/Preview.js
+++ src/client/js/components/Preview.js
@@ -30,14 +30,21 @@ class Preview extends Component {
     console.log(this.props)
     return (
       <div className='container'>
-        <Link to={`/post`} className='btn'>Go back!</Link>
-          <p>Name: {this.props.jobs.newJob.name}</p>
-          <p>Title: {this.props.jobs.newJob.title}</p>
-          <p>Description: {this.props.jobs.newJob.description}</p>
-          <p>City: {this.props.jobs.newJob.city}</p>
-          <p>Country: {this.props.jobs.newJob.country}</p>
-          <p>Website: {this.props.jobs.newJob.website}</p>
-        <Link to={`/`} onClick={this.handleSubmit.bind(this)} className='btn'>Submit</Link>
+        <div className='preview content'>
+          <Link to={`/post`} className='btn'>Go back!</Link>
+            <div className='info-container'>
+              <div className='title'>{this.props.jobs.newJob.title}</div>
+              <div className='name'>{this.props.jobs.newJob.name}</div>
+              <div className='location'>{this.props.jobs.newJob.city}, {this.props.jobs.newJob.country}</div>
+              <div className='website'>{this.props.jobs.newJob.website}</div>
+            </div>
+            <div className='job-description'>{this.props.jobs.newJob.description}</div>
+            <div className='apply'>
+              <h3>APPLY FOR THIS POSITION</h3>
+              <p>{this.props.jobs.newJob.how}</p>
+            </div>
+          <Link to={`/`} onClick={this.handleSubmit.bind(this)} className='btn'>Submit</Link>
+        </div>
       </div>
     )
   }
