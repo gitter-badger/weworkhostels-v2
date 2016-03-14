ce106784101007008e31951e2c61b627ec9fe259
itsanna <zhuanna93@gmail.com>
2016-03-14 14:47:50 -0700

    Using [].filter() in SearchBar component to filter jobs objects that have their key's value matching the input text...

--- src/client/js/components/SearchBar.js
+++ src/client/js/components/SearchBar.js
@@ -14,12 +14,33 @@ class SearchBar extends Component {
 
   handleKeyPress (e) {
     this.setState({term: e.target.value})
-    console.log("this is my state ", this.state)
-    console.log(this)
-    console.log(this.props.jobs)
+
+    var searchResults = this.props.jobs['list'].filter((el) => {
+      for (var key in el) {
+        if (typeof el[key] === 'number') {
+
+        } else {
+          if (el[key].includes(e.target.value)) {
+            return true
+          }       
+        }
+      }
+    })
+
+    this.setState({results: searchResults}) // maybe we should stick this into redux's single store
+
+    // Now we must re-render JobListings component with these search results.
+
+    // Shall we just replace the original jobs data (in Redux store) with this new result? 
+    // Everytime the data in the store changes, React will re-render the jobListings component.
+
+    // To do that we must dispatch an action...
+
+    console.log(this.state)
   }
 
   render () {
+    console.log(this)
     return (
       <div className='search-bar'>
         <input type="text"
